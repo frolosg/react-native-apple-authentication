aef3e1dbbc10ad996ab15ccfab65c26a31892bb2
sakshigoyalsg <63796507+sakshigoyalsg@users.noreply.github.com>
2020-04-22 13:46:20 +0530

    Update RNCSignInWithAppleButtonManager.m

--- ios/RNCSignInWithAppleButtonManager.m
+++ ios/RNCSignInWithAppleButtonManager.m
@@ -4,15 +4,25 @@
 @import AuthenticationServices;
 
 @interface RNCSignInWithAppleButtonManager : RCTViewManager
+
+@property (nonatomic, strong) RNCSignInWithAppleButton *appleSignInButton;
 @end
 
 @implementation RNCSignInWithAppleButtonManager
 
 RCT_EXPORT_MODULE(RNCSignInWithAppleButtonManager)
 
+
+RCT_EXPORT_METHOD(sendTapEvent)
+{
+  [self.appleSignInButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+}
+
 - (UIView *)view
 {
-  return [[RNCSignInWithAppleButton alloc] initWithAuthorizationButtonType:ASAuthorizationAppleIDButtonTypeDefault authorizationButtonStyle:ASAuthorizationAppleIDButtonStyleBlack];
+    
+  self.appleSignInButton = [[RNCSignInWithAppleButton alloc] initWithAuthorizationButtonType:ASAuthorizationAppleIDButtonTypeDefault authorizationButtonStyle:ASAuthorizationAppleIDButtonStyleBlack];
+    return self.appleSignInButton;
 }
 
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
